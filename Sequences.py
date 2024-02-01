8eb93da8792f0ba35ccb842cc8bfe9af57a3498d
adrian <ayikoadrian4@gmail.com>
2024-02-01 18:19:01 +0300

    updated python script

---
+++ Sequences.py
@@ -0,0 +1,22 @@
+# declaring , intializing and assigning variables
+y = 1 
+b = 2 
+c=3 
+print(f"The variables we have are {y}, {b},and {c} for reference")
+
+#trying out the if conditions 
+
+number = int(input("Enter a number: "))
+
+if(number > 0):
+    print(f"The number {number} is a positive number.")
+elif(number == 0):
+    print(f"{number} is neither positive nor negative.")
+else:
+    print(f"The number {number} is a negative number.")
+    
+#Data structures 
+
+names = ["mark", "sam" , "paulo"]
+
+print(names[0])
