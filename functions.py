c5612b1107b15e4ae385bae55afb765515195a28
adrian <ayikoadrian4@gmail.com>
2024-02-06 12:41:45 +0300

    The functions try out

---
+++ functions.py
@@ -0,0 +1,20 @@
+names = ["mark", "Aggrey", "Duku","Benjamin"]
+for name in names:
+    print(name)
+# Output: mark Aggrey Duku Benjamin
+for name in range(len(names)):
+    print(names[name])
+# Output: mark 0 Aggrey 1 Duku 2 Benjamin 3</s
+
+halls = {
+    "duku":"lumumba",
+    "Benjamin":"Livingstone",
+    "mark":"Mitchell",
+    "Aggrey":"Nsibiwa"
+}
+print(halls["mark"]) # Output: Mitchell
+
+if "Duku" in halls.keys():
+    print(halls["duku"])# Output: lumumba
+else:
+    print(halls["Benjamin"])# Output: livingstone
