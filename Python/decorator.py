961520a8aebd898f9b0b4f2229eec931b9f903a0
Ayiko Mark Adrian <ayikoadrian4@gmail.com>
2024-07-04 09:04:23 +0300

    Python/decorator.py

--- Python/decorator.py
+++ Python/decorator.py
@@ -1,9 +1,12 @@
 #using extensions to a class in another class
-def my_decorator(func):
+def my_decorator(func):#takes a function as an argument and returns a new function
     def wrapper():
-        print("I am a decorator")
+        print("I am about to decorate the Function")
+        func()
+        print("Done with the function")
+    return wrapper
 
 @my_decorator
-def say_hello():
+def hello():
     print("Hello")
-say_hello()
+hello()
